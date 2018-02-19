1f52e058e9e97501c829b458534fe87e322c81c5
YamAzoulay <ya5596@bs>
2018-02-19 14:34:39 +0200

    yam

--- app/src/main/java/com/example/yam35/yamwv/MainActivity.java
+++ app/src/main/java/com/example/yam35/yamwv/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     String URL, a, b, c;
     int a1;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
